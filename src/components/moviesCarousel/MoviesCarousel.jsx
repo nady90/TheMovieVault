451f955268b4d97e95545bea570e5c67a4d8ac34
Abanoub <naguib.abanoub@gmail.com>
2023-05-10 00:27:19 +0200

    Added scroll buttons

--- src/components/moviesCarousel/MoviesCarousel.jsx
+++ src/components/moviesCarousel/MoviesCarousel.jsx
@@ -61,17 +61,52 @@ const MoviesCarousel = ({
   }, [movies]);
 
   const handleScroll = (e) => {
-    // console.log(carousel.current.scrollLeft);
+    const maxScrollLeft =
+      carousel.current.scrollWidth - carousel.current.offsetWidth;
+
+    const safetyMargin = 40;
+
+    if (carousel.current.scrollLeft + safetyMargin >= maxScrollLeft) {
+      rightBtn.current.style.display = "none";
+    } else {
+      rightBtn.current.style.display = "block";
+    }
+
+    if (carousel.current.scrollLeft <= safetyMargin) {
+      leftBtn.current.style.display = "none";
+    } else {
+      leftBtn.current.style.display = "block";
+    }
   };
 
   const handleLeftBtnScroll = () => {
-    console.log("left", carousel.current.scrollLeft);
-    carousel.current.scrollLeft = carousel.current.scrollLeft - 20;
+    const distance = 160;
+    const step = 20;
+    const speed = 55;
+    let scrollAmount = 0;
+
+    const sliderTimer = setInterval(() => {
+      carousel.current.scrollLeft = carousel.current.scrollLeft - step;
+      scrollAmount = scrollAmount + step;
+      if (scrollAmount >= distance) {
+        clearInterval(sliderTimer);
+      }
+    }, speed);
   };
 
   const handleRightBtnScroll = () => {
-    console.log("right", carousel.current.scrollLeft);
-    carousel.current.scrollLeft = carousel.current.scrollLeft + 20;
+    const distance = 160;
+    const step = 20;
+    const speed = 55;
+    let scrollAmount = 0;
+
+    const sliderTimer = setInterval(() => {
+      carousel.current.scrollLeft = carousel.current.scrollLeft + step;
+      scrollAmount = scrollAmount + step;
+      if (scrollAmount >= distance) {
+        clearInterval(sliderTimer);
+      }
+    }, speed);
   };
 
   useEffect(() => {
@@ -80,15 +115,60 @@ const MoviesCarousel = ({
     rightBtn.current.addEventListener("click", handleRightBtnScroll);
   }, []);
 
+  useEffect(() => {
+    carousel.current.addEventListener("mouseover", () => {
+      const maxScrollLeft =
+        carousel.current.scrollWidth - carousel.current.offsetWidth;
+
+      const safetyMargin = 40;
+
+      if (carousel.current.scrollLeft + safetyMargin >= maxScrollLeft) {
+        // console.log("right");
+        rightBtn.current.style.display = "none";
+      } else {
+        rightBtn.current.style.display = "block";
+        rightBtn.current.classList.add("popup-btn");
+      }
+
+      if (carousel.current.scrollLeft <= safetyMargin) {
+        leftBtn.current.style.display = "none";
+      } else {
+        leftBtn.current.style.display = "block";
+      }
+    });
+  }, []);
+
   return (
     <div className={typesObject[type]}>
       {categoryTitle()}
       <button ref={leftBtn} className="scroll-left-btn">
-        Left
+        <svg
+          width="25"
+          height="43"
+          viewBox="0 0 25 43"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M21.2314 42.4628L0 21.2314L21.2314 0L25 3.76858L7.53716 21.2314L25 38.6943L21.2314 42.4628Z"
+            fill="black"
+          />
+        </svg>
       </button>
 
-      <button ref={rightBtn} className="scroll-right-btn">
-        Right
+      <button ref={rightBtn} className="scroll-right-btn popup-btn">
+        <svg
+          width="25"
+          height="43"
+          viewBox="0 0 25 43"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3.76858 42.4628L25 21.2314L3.76858 0L9.05282e-07 3.76858L17.4628 21.2314L9.05282e-07 38.6943L3.76858 42.4628Z"
+            fill="black"
+          />
+        </svg>
       </button>
 
       <div ref={carousel} className="scroller-container">
