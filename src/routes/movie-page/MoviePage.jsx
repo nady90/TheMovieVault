60791448b97fcb86ceeb46f302c71aebf3aacef0
Abanoub <naguib.abanoub@gmail.com>
2023-05-12 12:43:31 +0200

    Made the close svg button bigger on the movie logo container so that it becomes more obvious

--- src/routes/movie-page/MoviePage.jsx
+++ src/routes/movie-page/MoviePage.jsx
@@ -218,8 +218,8 @@ const MoviePage = () => {
 
                   <svg
                     className="close"
-                    width="40"
-                    height="40"
+                    width="60"
+                    height="60"
                     viewBox="0 0 24 24"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
