00e2e1b370fb80b814e4472409046b73ff009a97
Abanoub <naguib.abanoub@gmail.com>
2023-05-13 14:33:57 +0200

    Fixed deployment issues related to file names

--- src/routes/Underconstruction/Underconstruction.jsx
+++ src/routes/Underconstruction/Underconstruction.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Underconstruction.styles.scss";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 const Underconstruction = () => {
   return (
